42e60a66a279970f8282dcb601ce10a970a6d6cf
hiangae <hb402@nate.com>
2023-10-21 16:59:26 +0900

    개츠비 후디 테마 적용

---
+++ blog-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  title: "B.Y.HEO's Blog",
+  description: "개인 자료 정리 블로그",
+  author: "B.Y.HEO",
+  siteUrl: "https://hiangae.github.io/",
+  links: {
+    github: "https://github.com/hiangae",
+    linkedIn: "https://linkedin.com",
+    facebook: "https://www.facebook.com",
+    twitter: "https://twitter.com",
+    x: "https://x.com",
+    instagram: "https://www.instagram.com",
+    medium: "https://medium.com",
+    blogger: "https://blogger.com",
+    email: "mailto:hiangae@gmail.com",
+    resume: "",
+    link: "",
+  },
+  utterances: {
+    repo: "hiangae/hiangae.github.io",
+    type: "pathname",
+  },
+}
